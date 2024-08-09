1ad614f9c489233148a6c7e33564355e4e03ab43
saurab siwakoti <ggroxi1@gmail.com>
2024-08-09 20:28:05 +0545

    first commit

---
+++ public/scripts.js
@@ -0,0 +1,267 @@
+let userName;
+if(!localStorage.getItem(userName)){
+  userName = window.prompt("Enter user name")
+  localStorage.setItem('userName', userName)
+}else {
+  userName = localStorage.getItem(userName)
+}
+
+let isInCall = false
+
+const localVideoEl = document.getElementById("local-feed");
+const remoteVideoEl = document.getElementById("remote-feed");
+
+const hangUpBtn = document.getElementById("hangup-button");
+
+const socketList = document.getElementById("sockets");
+
+let peerConnection;
+
+const config = {
+  iceServers: [
+    {
+      urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"],
+    },
+  ],
+};
+
+let didCall = true;
+const renderSockets = (connectedSocketList) => {
+  socketList.innerHTML = "";
+  connectedSocketList.forEach((socket) => {
+    const newSocketElement = createNewSocketEl(socket);
+    socketList.appendChild = newSocketElement;
+  });
+};
+
+const socket = io.connect("https://192.168.18.16:8080", {
+  auth: {
+    userName,
+  },
+});
+
+let connectedSockets = [];
+let partner
+
+socket.on("socketListUpdated", (sockets) => {
+  connectedSockets = [];
+  for (let i = 0; i < sockets.length; i++) {
+    if (socket.id !== sockets[i].socketId) {
+      connectedSockets.push(sockets[i]);
+    }
+  }
+
+  socketList.innerHTML = "";
+  for (let i = 0; i < connectedSockets.length; i++) {
+    socketList.appendChild(createNewSocketEl(connectedSockets[i]));
+  }
+});
+
+const createNewSocketEl = (newSocket) => {
+  const newSocketElement = document.createElement("div");
+  newSocketElement.style.backgroundColor = "gray";
+
+  const newSocketName = document.createElement("p");
+  const callBtn = document.createElement("button");
+  callBtn.style.cursor = "pointer";
+
+  newSocketElement.appendChild(newSocketName);
+  newSocketElement.appendChild(callBtn);
+  callBtn.className = "callBtn";
+  newSocketName.textContent = `${newSocket.userName}`;
+  callBtn.textContent = "Call";
+
+  callBtn.addEventListener("click", (e) => {
+    call(newSocket);
+  });
+
+  return newSocketElement;
+};
+
+let localStream;
+let remoteStream = new MediaStream();
+
+let sendIceCandidateTo;
+
+const call = async (callSocket) => {
+  const socketToCall = {
+    userName: callSocket.userName,
+    socketId: callSocket.socketId,
+  };
+
+    sendIceCandidateTo = socketToCall.socketId;
+    await fetchUserMedia();
+
+    await createPeerConnection(config);
+
+    await createOffer(socketToCall);
+};
+
+hangUpBtn.addEventListener("click", (e) => {
+  // Emitting hangup
+  if(peerConnection){
+
+    socket.emit("hangUp" , partner)
+  }
+
+  hangUp() 
+});
+
+const hangUp = () => {
+  if (peerConnection) {
+
+    // closing the peer connection
+    peerConnection.close();
+    peerConnection = null
+
+    // stopping the streams 
+    remoteStream.getTracks().forEach((t) => {
+      t.stop()
+      remoteStream = null
+      remoteStream = new MediaStream()
+    });
+
+    localStream.getTracks().forEach((t) => {
+      t.stop()
+      localStream = null
+    })
+
+    isInCall = false
+  } else {
+    window.alert("not in an active call");
+  }
+}
+
+const fetchUserMedia = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      });
+      localVideoEl.srcObject = stream;
+      localStream = stream;
+      remoteVideoEl.srcObject = remoteStream;
+      resolve();
+    } catch (err) {
+      console.log(err);
+      reject();
+    }
+  });
+};
+
+const createPeerConnection = async (peerConfig) => {
+  peerConnection = await new RTCPeerConnection(peerConfig);
+
+  localStream.getTracks().forEach((track) => {
+    try {
+      peerConnection.addTrack(track, localStream);
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
+  });
+
+  peerConnection.addEventListener("icecandidate", (e) => {
+    const candidateInfo = {
+      candidate: e.candidate,
+      to: sendIceCandidateTo,
+    };
+    socket.emit("iceCandidate", candidateInfo);
+  });
+
+  peerConnection.addEventListener("track", (e) => {
+    e.streams[0].getTracks().forEach((t) => {
+      try {
+        remoteStream.addTrack(t, remoteStream);
+      } catch (err) {
+        console.log(`Error adding track: ${err}`);
+      }
+    });
+  });
+};
+
+const createOffer = async (socketToCall) => {
+  try {
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    const offerDescription = {
+      offer: offer,
+      socketToCall: socketToCall,
+    };
+    socket.emit("newOffer", offerDescription);
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+};
+
+socket.on("offerAwaiting", async (callOffer) => {
+  const offer = callOffer.offer;
+  const socketToCall = callOffer.socketToCall;
+  const callingSocket = callOffer.callingSocket;
+
+  if (
+    socketToCall.userName === userName &&
+    socketToCall.socketId === socket.id
+  ) {
+    if (window.confirm(`${callingSocket.userName} is calling`)) {
+      partner = {
+        socketId: callingSocket.socketId,
+        userName: callingSocket.userName
+      }
+      await fetchUserMedia();
+      await createPeerConnection(config);
+      sendIceCandidateTo = callingSocket.socketId;
+      await peerConnection.setRemoteDescription(
+        new RTCSessionDescription(offer)
+      );
+      const answer = await peerConnection.createAnswer({});
+
+      await peerConnection.setLocalDescription(answer);
+
+      const answerDescription = {
+        answerFrom: {
+          userName: userName,
+          socketId: socket.id,
+        },
+        answerTo: {
+          userName: callingSocket.userName,
+          socketId: callingSocket.socketId,
+        },
+        answer: answer,
+      };
+      didCall = false;
+      socket.emit("callAnswered", answerDescription);
+    } else {
+      socket.emit("callRejected", callOffer);
+    }
+  } else {
+    return;
+  }
+});
+
+socket.on("answerReceived", async (answerOffer) => {
+  partner = {
+    socketId: answerOffer.answerer.socketId,
+    userName: answerOffer.answerer.userName
+  }
+  await peerConnection.setRemoteDescription(answerOffer.answer);
+});
+
+socket.on("iceCandidateAdded", async (candidate) => {
+  if (peerConnection && candidate) {
+    await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+  }
+});
+
+socket.on('hungup', hungUpBy => {
+  window.alert(`${hungUpBy.userName} hung up on you`)
+  hangUp()
+})
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,103 @@
+const fs = require("fs");
+const express = require("express");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const https = require("https");
+const path = require("path");
+
+const app = new express();
+app.use(express.static(path.join(__dirname, "public")));
+
+const cert = fs.readFileSync("cert.crt");
+const key = fs.readFileSync("cert.key");
+
+const httpsServer = https.createServer({ key, cert }, app);
+
+const io = new Server(httpsServer, {
+  cors: {
+    origin: ["https://192.168.18.16/", "https://localhost/"],
+    methods: ["GET", "POST"],
+  },
+});
+
+let connectedSockets = [];
+
+io.on("connection", (socket) => {
+  const userName = socket.handshake.auth.userName;
+  console.log(
+    `Socket id: ${socket.id} connected successfully with the user name: ${userName}`
+  );
+
+  if (socket.id && userName) {
+    connectedSockets.push({
+      socketId: socket.id,
+      userName,
+    });
+  }
+  console.log(connectedSockets);
+  io.emit("socketListUpdated", connectedSockets);
+
+  socket.on("disconnect", () => {
+    const userName = socket.handshake.auth.userName;
+    console.log(
+      `Socket id: ${socket.id} disconnected with user name: ${userName}`
+    );
+
+    console.log(connectedSockets);
+
+    connectedSockets = connectedSockets.filter((s) => s.socketId !== socket.id);
+    socket.broadcast.emit("socketListUpdated", connectedSockets);
+  });
+
+  socket.on("newOffer", (offerDescription) => {
+    const offer = offerDescription.offer;
+    const socketToCall = offerDescription.socketToCall;
+    const callOffer = {
+      offer: offer,
+      socketToCall: socketToCall,
+      callingSocket: {
+        socketId: socket.id,
+        userName: userName,
+      },
+    };
+    socket.broadcast.emit("offerAwaiting", callOffer);
+  });
+
+  socket.on("callAnswered", (answerDescription) => {
+    const offerer = answerDescription.answerTo;
+    const answerer = answerDescription.answerFrom;
+    const answer = answerDescription.answer;
+
+    const answerOffer = {
+      offerer: offerer,
+      answerer: answerer,
+      answer: answer,
+    };
+
+    socket.to(offerer.socketId).emit("answerReceived", answerOffer);
+  });
+
+  socket.on("iceCandidate", (candidateInfo) => {
+    console.log(
+      `New ice candidate received: ${candidateInfo.candidate} from socket: ${socket.id}`
+    );
+    const candidate = candidateInfo.candidate;
+    const to = candidateInfo.to;
+    console.log(`Emitting the ice candidate to ${to}`);
+    socket.to(to).emit("iceCandidateAdded", candidate);
+  });
+
+  socket.on("hangUp", (partner) => {
+    console.log(`${userName} hung up on ${partner.socketId}`);
+    socket.to(partner.socketId).emit("hungup", {
+      socketId: socket.id,
+      userName: userName,
+    });
+
+  });
+
+});
+
+httpsServer.listen(8080, () => {
+  console.log("server up and running");
+});
